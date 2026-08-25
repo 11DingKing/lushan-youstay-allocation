fe70254244a4f7d276080f9beed6459afedf6281
ding <ding@ding.com>
2026-08-25 09:15:26 +0800

    R1 002-worker-v3 private test only

---
+++ internal/worker/task002_initial_error_test.go
@@ -0,0 +1,33 @@
+package worker_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/11DingKing/lushan-youstay-allocation/internal/clock"
+	"github.com/11DingKing/lushan-youstay-allocation/internal/domain"
+	"github.com/11DingKing/lushan-youstay-allocation/internal/worker"
+)
+
+type startupFailureHoldStore struct {
+	cancel context.CancelFunc
+	err    error
+}
+
+func (s startupFailureHoldStore) ExpireDueHolds(context.Context, time.Time, int) (int, error) {
+	s.cancel()
+	return 0, s.err
+}
+
+func TestRunKeepsRecoveringAfterInitialFailure(t *testing.T) {
+	sentinel := errors.New("recovery database unavailable")
+	ctx, cancel := context.WithCancel(context.Background())
+	tasks := &taskStore{byRole: map[string][]domain.OperationalTask{}, claimErr: map[string]error{}}
+	runner := worker.NewRunner(clock.NewManual(time.Now()), startupFailureHoldStore{cancel: cancel, err: sentinel}, &cleaner{}, tasks, worker.NewDispatchHandler(), time.Hour, testLogger())
+	err := runner.Run(ctx)
+	if !errors.Is(err, context.Canceled) {
+		t.Fatalf("worker stopped after transient recovery error = %v, want %v", err, context.Canceled)
+	}
+}
